97448a139474ae0e0baaa8741f8269e37d797d6e
MyProjects <deshpandepranav47@gmail.com>
2022-10-07 14:02:23 +0530

    called the overrriden method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,8 @@ import javax.sound.midi.Soundbank;
 public class Main {
     public static void main(String[] args) {
         B b = new B();
-        System.out.println(b.a.meth());
-        
-        System.out.println(b.meth());
+        b.a.meth();
+        b.meth();
     }
 }
 class A{
