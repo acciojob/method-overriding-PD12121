6c8069fca2d58896b17df75f046ae196c0712b9a
MyProjects <deshpandepranav47@gmail.com>
2022-10-07 13:58:02 +0530

    called the overrriden method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,4 +1,21 @@
-package com.driver;
+import javax.sound.midi.Soundbank;
 
-public class Main{
+public class Main {
+    public static void main(String[] args) {
+        B b = new B();
+        System.out.println(b.a.meth());
+        
+        System.out.println(b.meth());
+    }
+}
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    A a = new A();
+    String meth(){
+        return "Method is overridden in Extendend class B";
+    }
 }
