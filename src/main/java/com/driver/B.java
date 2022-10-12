524872850a29a2a78a8ec7956b73c81bffe2f80a
MyProjects <deshpandepranav47@gmail.com>
2022-10-12 19:31:15 +0530

    made chnages

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -3,7 +3,6 @@ package com.driver;
 public class B extends A {
     @Override
     public String meth() {
-        System.out.println(super.meth());
         return "Method is overridden in Extendend class B";
     }
 
