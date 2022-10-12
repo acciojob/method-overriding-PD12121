178f53be8b0716f47e9345f924dc2d94603b0542
MyProjects <deshpandepranav47@gmail.com>
2022-10-12 19:12:27 +0530

    made chnages

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -3,6 +3,7 @@ package com.driver;
 public class B extends A {
     @Override
     public String meth() {
+        System.out.println(super.meth());
         return "Method is overridden in Extendend class B";
     }
 }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,7 +5,7 @@ import javax.sound.midi.Soundbank;
 public class Main {
     public static void main(String[] args) {
         B b = new B();
-        b.meth();
+        System.out.println(b.meth());
     }
 }
 
