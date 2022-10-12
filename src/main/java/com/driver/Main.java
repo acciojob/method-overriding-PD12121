dae7c59495a4b24f53ef22b6232e499d94b1d3c6
MyProjects <deshpandepranav47@gmail.com>
2022-10-12 19:34:47 +0530

    made chnages

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,12 @@
 package com.driver;
 
 import javax.sound.midi.Soundbank;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         B b = new B();
-        b.meth();
+        System.out.println(b.meth());
     }
 }
 
