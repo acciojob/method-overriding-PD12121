761a6cd3e50346aea9e91f239fe0196ba6e8001e
MyProjects <deshpandepranav47@gmail.com>
2022-10-12 19:06:52 +0530

    made chnages

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,11 @@
+package com.driver;
+
 import javax.sound.midi.Soundbank;
 
 public class Main {
     public static void main(String[] args) {
         B b = new B();
-        b.a.meth();
         b.meth();
     }
 }
-class A{
-    String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    A a = new A();
-    String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
+
