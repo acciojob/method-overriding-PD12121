aadfbca08db6e31aef253c754ba2f5765bc8b1c4
MyProjects <deshpandepranav47@gmail.com>
2022-10-13 21:22:30 +0530

    made chnages

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         B b = new B();
-        System.out.println(b.meth());
+        b.meth();
     }
 }
 
